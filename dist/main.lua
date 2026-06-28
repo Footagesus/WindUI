1d482b77d60162fc0cefbefbd0cad1f854e08c6e
.ftgs <182904030+Footagesus@users.noreply.github.com>
2026-06-28 16:55:14 +0500

    build: 1.6.66

--- dist/main.lua
+++ dist/main.lua
@@ -4,7 +4,7 @@
     | |/ |/ / / _ \/ _  / /_/ // /  
     |__/|__/_/_//_/\_,_/\____/___/
     
-    v1.6.65  |  2026-06-26  |  Roblox UI Library for scripts
+    v1.6.65  |  2026-06-28  |  Roblox UI Library for scripts
     
     To view the source code, see the `src/` folder on the official GitHub repository.
     
